6df04be60af4f9b9ce47c2fbe1d735d42e898fe0
Artem Yur <artemonodon@gmail.com>
2026-03-30 11:45:12 +0200

    'Solution'

--- app/managers.py
+++ app/managers.py
@@ -11,27 +11,29 @@ class ActorManager:
 
     def create(self, first_name: str, last_name: str) -> None:
         self._connection.execute(
-            f"INSERT INTO {self.table_name} (first_name, last_name) VALUES (?, ?) ",
+            f"INSERT INTO {self.table_name} (first_name, last_name) "
+            "VALUES (?, ?) ",
             (first_name, last_name)
         )
         self._connection.commit()
 
     def all(self) -> list[Actor]:
         actor_cursor = self._connection.execute(
-            f"SELECT * FROM {self.table_name}"
+            f"SELECT * FROM {self.table_name} "
         )
         return [Actor(*row) for row in actor_cursor]
 
     def update(self, pk: int, new_first_name: str, new_last_name: str) -> None:
         self._connection.execute(
-            f"UPDATE {self.table_name} SET first_name=?, last_name=? WHERE id=?",
+            f"UPDATE {self.table_name} "
+            "SET first_name=?, last_name=? WHERE id=? ",
             (new_first_name, new_last_name, pk)
         )
         self._connection.commit()
 
     def delete(self, pk: int) -> None:
         self._connection.execute(
-            f"DELETE FROM {self.table_name} WHERE id=?",
+            f"DELETE FROM {self.table_name} WHERE id=? ",
             (pk,)
         )
         self._connection.commit()
